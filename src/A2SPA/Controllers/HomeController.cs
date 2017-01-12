3261ceccb72d46ecb0fd240e3e5db4df46530411
RobertDyball <robert.dyball@gmail.com>
2017-01-12 18:34:39 +1100

    cleaned up home page title, re-instated jquery for bootstrap javascript support

--- src/A2SPA/Controllers/HomeController.cs
+++ src/A2SPA/Controllers/HomeController.cs
@@ -6,20 +6,7 @@ namespace A2SPA.Controllers
     {
         public IActionResult Index()
         {
-            return View();
-        }
-
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
+            ViewData["Title"] = "Home";
             return View();
         }
 
