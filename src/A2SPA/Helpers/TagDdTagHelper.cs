64c7b516a47e4dd5c02520826450c3b708c08860
RobertDyball <robert.dyball@gmail.com>
2017-02-02 07:25:59 +1100

    Working tag helpers;
    issue: intellisense picks lower cased name in cshtml view

---
+++ src/A2SPA/Helpers/TagDdTagHelper.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace A2SPA.Helpers
+{
+    [HtmlTargetElement("tag-dd")]
+    public class TagDdTagHelper : TagHelper
+    {
+        /// <summary>
+        /// Name of data property 
+        /// </summary>
+        [HtmlAttributeName("for")]
+        public ModelExpression For { get; set; }
+
+        /// <summary>
+        /// Option: directly set display format using Angular 2 pipe and pipe format values
+        /// </summary>
+        ///<remarks>This attribute sets both pipe type and the pipe filter parameters.
+        /// For simple formatting of common data types <seealso cref="Format"/>.
+        /// Numeric formats for decimal or percent in Angular use a string with the following format: 
+        /// a.b-c where:
+        ///     a = minIntegerDigits is the minimum number of integer digits to use.Defaults to 1.
+        ///     b = minFractionDigits is the minimum number of digits after fraction.Defaults to 0.
+        ///     c = maxFractionDigits is the maximum number of digits after fraction.Defaults to 3.
+        /// </remarks>
+        /// <example>
+        /// to format a decimal value as a percentage use "|percent" for the default Angular
+        /// or for a custom percentage value eg. "| percent:'1:3-5' 
+        /// </example>
+        [HtmlAttributeName("pipe")]
+        public string Pipe { get; set; } = null;
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            var pipe = string.IsNullOrEmpty(Pipe) ? string.Empty : Pipe;
+
+            var labelTag = new TagBuilder("label");
+            labelTag.InnerHtml.Append(For.Metadata.Description);
+            labelTag.AddCssClass("control-label");
+
+            var pTag = new TagBuilder("p");
+            pTag.AddCssClass("form-control-static");
+            pTag.InnerHtml.Append("{{" + For.CamelizedName() + pipe + "}}");
+
+            output.TagName = "div";
+            output.Attributes.Add("class", "form-group");
+
+            output.Content.AppendHtml(labelTag);
+            output.Content.AppendHtml(pTag);
+        }
+    }
+}
--- src/A2SPA/ViewModels/TestData.cs
+++ src/A2SPA/ViewModels/TestData.cs
@@ -4,22 +4,23 @@ namespace A2SPA.ViewModels
 {
     public class TestData
     {
+        [Display(Description = "Username")]
         public string Username { get; set; }
 
+        [Display(Description = "Payment Amount (in dollars)")]
         [DataType(DataType.Currency)]
         public decimal Currency { get; set; }
 
-
         [Required, RegularExpression(@"([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})", ErrorMessage = "Please enter a valid email address.")]
         [EmailAddress]
-        [Display(Name = "EmailAddress", ShortName = "Email", Prompt = "Email Address")]
+        [Display(Description = "Username", Name = "EmailAddress", ShortName = "Email", Prompt = "Email Address")]
         [DataType(DataType.EmailAddress)]
         public string EmailAddress { get; set; }
 
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
-        [Display(Name = "Password")]
+        [Display(Description = "Password", Name = "Password")]
         public string Password { get; set; }
 
     }
