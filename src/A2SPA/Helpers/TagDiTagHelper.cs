71140db4491f72b98f1489769fc42149c5e6df37
RobertDyball <robert.dyball@gmail.com>
2017-02-16 17:21:20 +1100

    WIP; further progress to a working data input tag helper

--- src/A2SPA/Helpers/TagDiTagHelper.cs
+++ src/A2SPA/Helpers/TagDiTagHelper.cs
@@ -18,6 +18,8 @@ namespace A2SPA.Helpers
 
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
+            var dataType = ((DefaultModelMetadata)For.Metadata).DataTypeName;
+
             var labelTag = new TagBuilder("label");
             labelTag.InnerHtml.Append(For.Metadata.Description);
             labelTag.MergeAttribute("for", For.Name.Camelize());
@@ -25,7 +27,22 @@ namespace A2SPA.Helpers
 
             var inputTag = new TagBuilder("input");
             inputTag.AddCssClass("form-control");
-            inputTag.MergeAttribute("type", "password");
+
+            switch (dataType)
+            {
+                case "Password":
+                    inputTag.MergeAttribute("type", dataType);
+                    break;
+
+                case "Currency":
+                    inputTag.MergeAttribute("type", "number");
+                    break;
+
+                default:
+                    inputTag.MergeAttribute("type", "text");
+                    break;
+            }
+
             inputTag.MergeAttribute("id", For.Name.Camelize());
             inputTag.MergeAttribute("name", For.Name.Camelize());
             inputTag.MergeAttribute("placeholder", For.Metadata.Description);
@@ -52,7 +69,25 @@ namespace A2SPA.Helpers
             output.Attributes.Add("class", "form-group");
 
             output.Content.AppendHtml(labelTag);
-            output.Content.AppendHtml(inputTag);
+
+            // wrap the input tag with an input group, if needed
+            switch (dataType)
+            {
+                case "Currency":
+                    var divInputGroup = new TagBuilder("div");
+                    divInputGroup.MergeAttribute("class", "input-group");
+                    var spanInputGroupAddon = new TagBuilder("span");
+                    spanInputGroupAddon.MergeAttribute("class", "input-group-addon");
+                    spanInputGroupAddon.InnerHtml.Append("$");
+                    divInputGroup.InnerHtml.AppendHtml(spanInputGroupAddon);
+                    divInputGroup.InnerHtml.AppendHtml(inputTag);
+                    output.Content.AppendHtml(divInputGroup);
+                    break;
+
+                default:
+                    output.Content.AppendHtml(inputTag);
+                    break;
+            }
         }
     }
 }
