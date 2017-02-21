aabb0a8ea63d7a31d694d4776c9fe964ec0d7c14
Robert Dyball <robert_dyball@hotmail.com>
2017-02-21 22:48:28 +1100

    added further comments to tag helper

--- src/A2SPA/Helpers/TagDiTagHelper.cs
+++ src/A2SPA/Helpers/TagDiTagHelper.cs
@@ -17,22 +17,27 @@ namespace A2SPA.Helpers
 
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
+            // get metadata names, property name and data type
             var metadata = ((DefaultModelMetadata)For.Metadata);
             var propertyName = For.Name.Camelize();
             var dataType = metadata.DataTypeName;
 
+            // find best fit for labels and descriptions
             var shortLabelName = metadata.DisplayName ?? this.For.Name.Humanize();
             var labelName = metadata.Placeholder ?? shortLabelName;
             var description = For.Metadata.Description ?? labelName;
 
+            // generate the label, point to the data entry input control
             var labelTag = new TagBuilder("label");
             labelTag.InnerHtml.Append(description);
             labelTag.MergeAttribute("for", propertyName);
             labelTag.AddCssClass("control-label");
 
+            // add the input control; TODO: add textarea, date picker support
             var inputTag = new TagBuilder("input");
             inputTag.AddCssClass("form-control");
 
+            // TODO: further expand datatypes here
             switch (dataType)
             {
                 case "Password":
@@ -48,15 +53,18 @@ namespace A2SPA.Helpers
                     break;
             }
 
+            // common attributes for data input control here
             inputTag.MergeAttribute("id", propertyName);
             inputTag.MergeAttribute("name", propertyName);
             inputTag.MergeAttribute("placeholder", shortLabelName);
             inputTag.MergeAttribute("#" + propertyName, "ngModel");
 
+            // set up validation conditional DIV here; only show error if an error in data entry
             TagBuilder validationBlock = new TagBuilder("div");
             validationBlock.MergeAttribute("*ngIf", string.Format("{0}.errors", propertyName));
             validationBlock.MergeAttribute("class", "alert alert-danger");
 
+            // Handle minimum, maximum, required, regex and other validation. TODO: refactor common code out
             if (metadata.HasMinLengthValidation())
             {
                 var minLength = metadata.MinLength();
@@ -99,15 +107,20 @@ namespace A2SPA.Helpers
                 inputTag.Attributes.Add("required", "required");
             }
 
+            // bind angular data model to the control,
             inputTag.MergeAttribute("[(ngModel)]", For.CamelizedName());
+
+            // TODO: if adding say text area, you want closing tag. For input tag you do not have closing or self-closing
             inputTag.TagRenderMode = TagRenderMode.StartTag;
 
+            // now generate the outer wrapper for the form group, get ready to start filling it with content prepared above
             output.TagName = "div";
             output.Attributes.Add("class", "form-group");
 
+            // first the label
             output.Content.AppendHtml(labelTag);
 
-            // wrap the input tag with an input group, if needed
+            // Some input controls use bootstrap "input group"- wrap the input tag with an input group, if needed
             switch (dataType)
             {
                 case "Currency":
@@ -125,10 +138,12 @@ namespace A2SPA.Helpers
                     break;
 
                 default:
+                    // most of the time we simply append the input controls prepared above
                     output.Content.AppendHtml(inputTag);
                     break;
             }
 
+            // add the validation prepared earlier, to the end, last of all
             output.Content.AppendHtml(validationBlock);
         }
     }
