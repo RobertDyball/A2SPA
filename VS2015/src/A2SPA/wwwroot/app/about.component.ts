1826efab1cda0f0d7bd5a1d5ddf7f72f5cad0ecd
Robert Dyball <robert_dyball@hotmail.com>
2017-03-09 21:46:09 +1100

    Added VS2017 version beside the VS2015 version
    part 1 of Code Project series

---
+++ VS2015/src/A2SPA/wwwroot/app/about.component.ts
@@ -0,0 +1,9 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-about',
+    templateUrl: '/partial/aboutComponent'
+})
+
+export class AboutComponent {
+}
---
+++ VS2017/A2SPA/wwwroot/app/main.ts
@@ -0,0 +1,8 @@
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app.module';
+
+//platformBrowserDynamic().bootstrapModule(AppModule);
+platformBrowserDynamic().bootstrapModule(AppModule)
+    .then((success: any) => console.log('App bootstrapped'))
+    .catch((err: any) => console.error(err));
