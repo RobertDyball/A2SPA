1f692a7bea1c1cd3e600476fe67cfe5b8b87ae6d
RobertDyball <robert.dyball@gmail.com>
2017-04-04 21:18:02 +1000

    part 7, publishing to IIS using VS2015

--- Create_sql_user.sql
+++ Create_sql_user.sql
@@ -1,2 +1,4 @@
+-- see: https://blogs.msdn.microsoft.com/sqlexpress/2011/12/08/using-localdb-with-full-iis-part-2-instance-ownership/
+-- create a user for SQL Express
 create login [IIS APPPOOL\a2spa] from windows;
 exec sp_addsrvrolemember N'IIS APPPOOL\a2spa', sysadmin
