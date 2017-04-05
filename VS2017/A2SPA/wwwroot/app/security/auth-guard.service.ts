bd7209d15e441f9fb7249cb40452e480a92ad922
Robert Dyball <robert.dyball@amal.com.au>
2017-04-05 14:03:32 +1000

    further changes to get VS2017 project ready for publishing to IIS

--- VS2017/A2SPA/wwwroot/app/security/auth-guard.service.ts
+++ VS2017/A2SPA/wwwroot/app/security/auth-guard.service.ts
@@ -10,7 +10,7 @@ export class AuthGuard implements CanActivate {
 
     canActivate() {
         if (!this.authService.loggedIn()) {
-            this.router.navigate(['/login']);
+            this.router.navigate(['login']);
             return false;
         }
         return true;
