21e090dd871ffc3a28578bf5fe4af4cc996e910d
RobertDyball <robert.dyball@gmail.com>
2017-04-07 07:05:51 +1000

    WIP hard coded fixes to get around .ts files in obj folder during build and publish
    todo: add macros

--- VS2017/A2SPA/wwwroot/wwwroot/app/models/errorResponse.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var ErrorResponse = (function () {
-    function ErrorResponse() {
-    }
-    return ErrorResponse;
-}());
-exports.ErrorResponse = ErrorResponse;
-//# sourceMappingURL=errorResponse.js.map
