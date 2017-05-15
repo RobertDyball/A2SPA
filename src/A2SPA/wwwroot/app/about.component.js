83868a8cdd91b14cc43edfe8c24623f66e520fc6
RobertDyball <robert.dyball@gmail.com>
2017-05-15 17:40:04 +1000

    Updated VS2015 solution to Angular 4.0

--- src/A2SPA/wwwroot/app/about.component.js
+++ src/A2SPA/wwwroot/app/about.component.js
@@ -5,9 +5,6 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-var __metadata = (this && this.__metadata) || function (k, v) {
-    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
-};
 var core_1 = require("@angular/core");
 var AboutComponent = (function () {
     function AboutComponent() {
@@ -18,8 +15,7 @@ AboutComponent = __decorate([
     core_1.Component({
         selector: 'my-about',
         templateUrl: '/partial/aboutComponent'
-    }),
-    __metadata("design:paramtypes", [])
+    })
 ], AboutComponent);
 exports.AboutComponent = AboutComponent;
 //# sourceMappingURL=about.component.js.map
