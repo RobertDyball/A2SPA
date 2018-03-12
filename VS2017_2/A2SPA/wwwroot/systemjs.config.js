5c4c475b9e475604b7005fdfb147677f292e48b9
Robert Dyball <robert.dyball@gmail.com>
2018-03-12 18:15:47 +1100

    updated npm packages

--- VS2017_2/A2SPA/wwwroot/systemjs.config.js
+++ VS2017_2/A2SPA/wwwroot/systemjs.config.js
@@ -31,7 +31,7 @@
             // other libraries
             'rxjs': 'npm:rxjs',
             'angular-in-memory-web-api': 'npm:angular-in-memory-web-api/bundles/in-memory-web-api.umd.js',
-            'ngx-toastr': 'npm:ngx-toastr/toastr.umd.js'
+            'ngx-toastr': 'npm:ngx-toastr/bundles/ngx-toastr.umd.js'
         },
         // packages tells the System loader how to load when no filename and/or no extension
         packages: {
